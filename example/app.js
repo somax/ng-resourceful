65657400a2a3956724def4bcbf77ad3b473142f7
somax <somaxj@163.com>
2016-08-15 13:38:41 +0800

    chore: tab => space

--- example/app.js
+++ example/app.js
@@ -1,37 +1,42 @@
-	angular.module('demoApp', ['ngResourceful'])
+    angular.module('demoApp', ['ngResourceful'])
         .factory('ResrOrgs', ResrOrgs)
         .controller('mainController', mainController);
 
-	function ResrOrgs(Resourceful) {
-		return new Resourceful({ 
-                     uri : '/api/orgs',
-                     isArray : true
-                });
-	}
- 	ResrOrgs.$injector = ['Resourceful'];
+    function ResrOrgs(Resourceful) {
+        return new Resourceful({
+            uri: '/api/orgs',
+            isArray: true
+        });
+    }
+    ResrOrgs.$injector = ['Resourceful'];
 
- 	function mainController(ResrOrgs,Resourceful) {
- 		var mc = this;
- 		mc.orgs = ResrOrgs;
- 		ResrOrgs.setData([{id:123}])
- 		ResrOrgs.get().then(function () {
- 			console.log('success');
- 		},function () {
- 			console.error('error');
- 		})
+    function mainController(ResrOrgs, Resourceful) {
+        var mc = this;
+        mc.orgs = ResrOrgs;
+        ResrOrgs.setData([{
+            id: 123
+        }])
+        ResrOrgs.get().then(function() {
+            console.log('success');
+        }, function() {
+            console.error('error');
+        })
 
-        mc.getThis = function (org) {
+        mc.getThis = function(org) {
 
-            if(!org.isResource){
-                Resourceful({target:org, uri:'/api/orgs/'+org.id})
+            if (!org.isResource) {
+                Resourceful({
+                    target: org,
+                    uri: '/api/orgs/' + org.id
+                })
             }
 
-            org.get().then(function(){
+            org.get().then(function() {
                 console.log(org);
                 mc.currentOrg = org;
             })
 
         }
 
- 	}
-    mainController.$injector = ['ResrOrgs','Resourceful'];
+    }
+    mainController.$injector = ['ResrOrgs', 'Resourceful'];
